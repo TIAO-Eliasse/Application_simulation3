04dbad6dd52330ba40257825c496de8c2d716159
TIAO-Eliasse <eliassetiao@gmail.com>
2025-01-01 00:03:57 +0100

    Add files via upload

---
+++ Application_MES/Accueil.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.title("Application Simulation ")
+st.write("## Mecanisme de transmissions des flucutuations des prix de pétroles dans l'économie de la Guinée Equatoriale")
+
+st.markdown("Cette application a été par:")
+
+st.write(" TIAO Eliasse")
+st.write("Amos Santiago")
+
+st.markdown("""
+    <style>
+    .stButton button {background-color: green;
+        color: white;
+        padding-top: 20px;
+        padding-bottom:40px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+#if st.button("Continuez"):
+    #st.switch_page("pages/1_AnalyseExploratoire.py")
---
+++ Application_MES/pages/2_Simulation.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.write("Bienvenue dans la partie simulation")
